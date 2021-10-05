5003ee1991f912b731f2a218f203baddf16912de
Danvar03 <daniela.03v@gmail.com>
2021-10-05 16:39:46 -0500

    Se realiza multiplicacion y diviion

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -1,16 +1,32 @@
-import java.util.function.BiFunction;
+
+import java.util.function.IntBinaryOperator;
+import java.util.stream.IntStream;
 
 
 public class calculator {
 
+    static IntBinaryOperator suma = (a, b) -> a + b;
+    static IntBinaryOperator resta = (a, b) -> a - b;
+    static IntBinaryOperator mult = (a, b) -> {
+        return IntStream.range(0, Math.abs(b) + 1)
+                .reduce((acumulado, number) ->
+                        suma.applyAsInt(a, acumulado)).getAsInt();
+
+    };
+    static IntBinaryOperator div = (a,b) -> {
+       return IntStream.range(0,Math.abs(a) )
+                .reduce((acumulador, numero) ->
+                        mult.applyAsInt(numero, b) <= a ? suma.applyAsInt(acumulador, 1) : acumulador).getAsInt();
+
+    };
+
 
     public static void main(String[] args) {
-        BiFunction<Integer, Integer, Integer> suma = (a, b) -> a + b;
-        BiFunction<Integer, Integer, Integer> resta = (a, b) -> a - b;
-
-        System.out.println(suma.apply(5, 2));
-        System.out.println(resta.apply(5, 2));
-
-
+        System.out.println("__________ Calculadora con PF __________");
+        System.out.println(suma.applyAsInt(3, 2));
+        System.out.println(resta.applyAsInt(3, 2));
+        System.out.println(mult.applyAsInt(-5, +10));
+        System.out.println(div.applyAsInt(-50,2));
     }
+
 }
