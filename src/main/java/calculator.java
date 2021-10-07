b09b26633f834b2b437743303b7fed91d17dd56c
Danvar03 <daniela.03v@gmail.com>
2021-10-07 10:35:06 -0500

    Se realiza validaciones

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -7,10 +7,21 @@ public class calculator {
 
     static IntBinaryOperator suma = (a, b) -> a + b;
     static IntBinaryOperator resta = (a, b) -> a - b;
-    static IntBinaryOperator mult = (a, b) -> {
+    static IntBinaryOperator mult = (a, b)-> {
+        if (b < 0 && a > 0){
+            int c = a;
+            a=b;
+            b=c;
+        }
+        if (b < 0 && a < 0){
+            a =-a;
+        }
+        int finalA = a;
         return IntStream.range(0, Math.abs(b) + 1)
-                .reduce((acumulado, number) ->
-                        suma.applyAsInt(a, acumulado)).getAsInt();
+                .reduce((acumalated, number) -> suma
+                        .applyAsInt(finalA, acumalated))
+                .getAsInt();
+
 
     };
     static IntBinaryOperator div = (a,b) -> {
@@ -25,7 +36,7 @@ public class calculator {
         System.out.println("__________ Calculadora con PF __________");
         System.out.println(suma.applyAsInt(3, 2));
         System.out.println(resta.applyAsInt(3, 2));
-        System.out.println(mult.applyAsInt(-5, +10));
+        System.out.println(mult.applyAsInt(-5, -10));
         System.out.println(div.applyAsInt(-50,2));
     }
 
